cbe444b0cf4264d36aa6e93fdefeefd1d0f83706
Hr13 <heiko.raab@uni-ulm.de>
2020-10-20 20:45:57 +0200

    added the possibility to add pieces in to the board

--- main.hs
+++ main.hs
@@ -44,11 +44,24 @@ printLine :: (Eq a, Num a) => [Piece] -> a -> IO ()
 printLine l n = printLineH l (getDefaultLine n)
 
 printLineH :: [Piece] -> [Char] -> IO ()
-printLineH [] s = print s
+printLineH [] s = putStrLn s
 printLineH (l:ls) s =
                 let currentLine = tradeInPiece s l
                 in printLineH ls currentLine
 
+placePiece :: [[Piece]] -> Piece -> (Int, Int) -> [[Piece]]
+placePiece [] _ _ = error "placing the new piece wasn't possible because the row wasn't valid"
+placePiece (line:board) p (y,x) | y == 0 = (placePieceInLine line p x) : board
+                                | y > 0 = line : placePiece board p ((y-1),x)
+
+placePieceInLine :: [Piece] -> Piece -> Int -> [Piece]
+placePieceInLine [] _ _ = error "placing the new piece wasn't possible because the column wasn't valid"
+placePieceInLine (l:ls) p x | x == 0 && l == Blank = p : ls
+                            | x > 0 = l : placePieceInLine ls p (x-1)
+                            | l /= Blank = error "coudln't place piece because field wasn't empty"
 
 main :: IO ()
-main = printBoard startBoard
+main = do
+        let b = startBoard
+        printBoard b
+        printBoard $ placePiece (placePiece b Black (3,3)) White (3,2)
